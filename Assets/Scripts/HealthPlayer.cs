4d52aeba372966c96d7db2abff0163814ed0c4c2
Vadim21school <vadikburduzha@yandex.ru>
2023-07-30 23:29:13 +0300

    Update HealthPlayer.cs

--- Assets/Scripts/HealthPlayer.cs
+++ Assets/Scripts/HealthPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine.Events;
 [RequireComponent(typeof(HealthBar))]
 public class HealthPlayer : MonoBehaviour
 {
-    [SerializeField] private UnityEvent changed;
+    [SerializeField] private UnityEvent _changed;
     [SerializeField] private float _amountOfChange;
     [SerializeField] private float _minAmount;
     [SerializeField] private float _maxAmount;
@@ -23,13 +23,13 @@ public class HealthPlayer : MonoBehaviour
         _currentValue = Amount + _amountOfChange;
 
         Amount = Mathf.Clamp(_currentValue, _minAmount, _maxAmount);
-        changed.Invoke();
+        _changed.Invoke();
     }
 
     public void ReduceHealth()
     {
         _currentValue = Amount - _amountOfChange;
         Amount = Mathf.Clamp(_currentValue, _minAmount, _maxAmount);
-        changed.Invoke();
+        _changed.Invoke();
     }
 }
